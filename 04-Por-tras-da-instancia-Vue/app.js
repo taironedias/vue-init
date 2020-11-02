0d99f0c3c9c4210314dbd75277654f3c6187a622
Tairone Dias <taironecdias@gmail.com>
2020-11-02 14:25:14 -0300

    060 Usando refs

--- 04-Por-tras-da-instancia-Vue/app.js
+++ 04-Por-tras-da-instancia-Vue/app.js
@@ -1,17 +1,19 @@
 let vm = new Vue({
     el: '#app',
     data: {
-        titulo: 'Curso de Vue.js',
-        usuario: {}
+        titulo: 'Curso de Vue.js'
+    },
+    methods: {
+        alterarTitulo: function(event) {
+            this.$refs.myTitle.innerText = 'Vue por refs';
+            this.$refs.myTitle.style.color = '#f90';
+            /* Fazendo dessa forma, o Vue perde a referência da interpolação, logo, não será possível alterar a o valor do titulo pela propriedade data, como no exemplo abaixo: */
+            let self = this;
+            setTimeout(() => {
+                self.titulo = 'Vue pela propriedade data'
+            }, 3000);
+        }
     }
 });
 
 console.log(vm);
-setTimeout(() => {
-    vm.newProperty = 'Nova propriedade'; // NÃO REATIVO, porém como estou chamado o comando abaixo, o valor dessa propriedade acaba sendo exibida no dom. ou seja, como é não reativo essa propriedade fica dependente de qualquer alteração na interface, para que seu valor seja exibibo
-    Vue.set(vm.usuario, 'nome', 'Tairone Dias');
-    setTimeout(() => {
-        vm.newProperty = 'Nova propriedade alterada.';
-        Vue.set(vm.usuario, 'email', 'tairone@email.com');
-    }, 2000);
-}, 2000);
