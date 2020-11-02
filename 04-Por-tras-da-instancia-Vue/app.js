ccb990169258c1335231fba6945395f80c832ff1
Tairone Dias <taironecdias@gmail.com>
2020-11-02 15:23:46 -0300

    066 Lifecycle Hooks do Vue.js

--- 04-Por-tras-da-instancia-Vue/app.js
+++ 04-Por-tras-da-instancia-Vue/app.js
@@ -1,12 +1,43 @@
-Vue.component('meu-component', {
-    template: '<h2>Curso de Vue.js</h2>'
-})
-
-let vm1 = new Vue({
-    el: '#app1'
+let vm = new Vue({
+    // el: '#app',
+    data: {
+        titulo: 'Curso de Vue.js'
+    },
+    methods: {
+        alterarTitulo: function(event) {
+            this.titulo = 'Curso de Vue.js, Vuex e VueRouter'
+        },
+        destruindoInstancia: function(event) {
+            this.$destroy();
+        }
+    },
+    beforeCreate: function() {
+        console.log('01 - beforeCreate');
+    },
+    created: function() {
+        console.log('02 - created');
+    },
+    beforeMount: function() {
+        console.log('03 - beforeMount');
+    },
+    mounted: function() {
+        console.log('04 - mounted');
+    },
+    beforeUpdate: function() {
+        console.log('05 - beforeUpdate');
+    },
+    updated: function() {
+        console.log('06 - updated');
+    },
+    beforeDestroy: function() {
+        console.log('07 - beforeDestroy');
+    },
+    destroyed: function() {
+        console.log('08 - destroyed');
+    },
 });
 
-let vm2 = new Vue({
-    el: '#app2',
-    template: '<div><h2>Instância 02</h2><meu-component></meu-component></div>'
-});
+/* Visualizando no log que o beforeMount e mounted sera executado após 3 segundos */
+setTimeout(() => {
+    vm.$mount('#app');
+}, 3000);
