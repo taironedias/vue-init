0f2179e8e3d9c794011fd883fc2fe4283e6bd7e5
Tairone Dias <taironecdias@gmail.com>
2020-11-08 22:12:46 -0300

    boas praticas de nomeacao de components

---
+++ with-docker/code/vue-components-intro/src/AppHeader.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Curso de Vue.js</h1>
+</template>
+/* O PascalCase é o padrão para criação de components Vue, porém pode-se utilizar o kebab-case */
--- with-docker/code/vue-components-intro/src/main.js
+++ with-docker/code/vue-components-intro/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 
-import App from './App.vue';
+// import App from './App.vue';
+import AppHeader from './AppHeader.vue';
 
-// Vue.component('app-contador', AppContador)
+Vue.component('AppHeader', AppHeader)
 
 new Vue({
-  render: h => h(App)
+  // render: h => h(App)
 }).$mount('#app')
