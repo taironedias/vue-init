60f575cfd0bb3afbffcf930ad68f5fa4cc3b33ef
Tairone Dias <taironecdias@gmail.com>
2020-11-08 21:55:00 -0300

    criando novos component (localmente)

--- with-docker/code/vue-components-intro/src/main.js
+++ with-docker/code/vue-components-intro/src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 
 import App from './App.vue';
-import AppContador from './AppContador.vue';
 
-Vue.component('app-contador', AppContador)
+// Vue.component('app-contador', AppContador)
 
 new Vue({
-  el: '#app',
   render: h => h(App)
-})
+}).$mount('#app')
