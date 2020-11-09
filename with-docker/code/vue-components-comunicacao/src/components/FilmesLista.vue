e636e7e844be2dfaf8b4b1335563c65f0c8fdfc9
Tairone Dias <taironecdias@gmail.com>
2020-11-09 20:05:48 -0300

    criando components necessarios

---
+++ with-docker/code/vue-components-comunicacao/src/components/FilmesLista.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row">
+        <!-- column 1 -->
+        <div class="col-8">
+            <h2>Filmes</h2>
+
+            <div class="list-group list-group-flush">
+                <FilmesListaItem/>
+                <FilmesListaItem/>
+                <FilmesListaItem/>
+            </div>
+        </div>
+
+        <!-- column 2 -->
+        <div class="col-4">
+            <FilmesListaItemInfo/>
+        </div>
+    </div>
+</template>
+
+<script>
+import FilmesListaItem from './FilmesListaItem.vue';
+import FilmesListaItemInfo from './FilmesListaItemInfo.vue';
+
+export default {
+    components: {
+        FilmesListaItem,
+        FilmesListaItemInfo
+    }
+}
+</script>
---
+++ with-docker/code/vue-components-comunicacao/src/components/FilmesListaItemEditar.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h2>Editar Filme</h2>
+
+        <div class="form-group">
+            <label for="">Título</label>
+            <input type="text" class="form-control" placeholder="Insira o título">
+        </div>
+    </div>
+</template>
