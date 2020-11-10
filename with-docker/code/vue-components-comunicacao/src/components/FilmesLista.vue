f0dfb0511442902d34a54f8dea67710d76eb9595
Tairone Dias <taironecdias@gmail.com>
2020-11-10 20:48:28 -0300

    custom Events passando dados do component filho para o component pai

--- with-docker/code/vue-components-comunicacao/src/components/FilmesLista.vue
+++ with-docker/code/vue-components-comunicacao/src/components/FilmesLista.vue
@@ -6,9 +6,10 @@
 
             <ul class="list-group list-group-flush">
                 <FilmesListaItem
-                    v-for="filme in filmes"
-                    :key="filme.id"
-                    :filme="filme"
+                    v-for="movie in movies"
+                    :key="movie.id"
+                    :movie="movie"
+                    @movieSelected="movieSelected = $event"
                 />
             </ul>
         </div>
@@ -31,12 +32,13 @@ export default {
     },
     data() {
         return {
-            filmes: [
+            movies: [
                 { id: 1, title: 'Capitão Marvel', year: 2019, director: 'Stan Lee'},
                 { id: 2, title: 'Vingadores: Guerra Infinita', year: 2019, director: 'Stan Lee'},
                 { id: 3, title: 'Homem-Aranha: Longe de Casa', year: 2019, director: 'Stan Lee'},
                 { id: 4, title: 'Viúva Negra', year: 2021, director: 'Stan Lee'}
-            ]
+            ],
+            movieSelected: undefined
         }
     }
 }
--- with-docker/code/vue-components-comunicacao/src/components/FilmesListaItem.vue
+++ with-docker/code/vue-components-comunicacao/src/components/FilmesListaItem.vue
@@ -1,16 +1,21 @@
 <template>
     <li class="list-group-item">
-        <span>{{ filme.title }} ({{ filme.year }})</span>
-        <button class="btn btn-success float-right">Selecionar</button>
+        <span>{{ movie.title }} ({{ movie.year }})</span>
+        <button class="btn btn-success float-right" @click="chooseMovie">Selecionar</button>
     </li>
 </template>
 
 <script>
 export default {
     props: {
-        filme: {
+        movie: {
             type: Object,
             required: true
+        }
+    },
+    methods: {
+        chooseMovie() {
+            this.$emit('movieSelected', this.movie)
         }
     }
 }
