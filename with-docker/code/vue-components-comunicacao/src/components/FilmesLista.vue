ddf65d031751dde66b7b77e528af8147ba449838
Tairone Dias <taironecdias@gmail.com>
2020-11-10 20:31:17 -0300

    atributos Non-prop

--- with-docker/code/vue-components-comunicacao/src/components/FilmesLista.vue
+++ with-docker/code/vue-components-comunicacao/src/components/FilmesLista.vue
@@ -5,10 +5,14 @@
             <h2>Filmes</h2>
 
             <ul class="list-group list-group-flush">
-                <!-- Essa é a forma simple, pois o objeto filme possue poucos atributos. -->
-                <!-- <FilmesListaItem v-for="filme in filmes" :key="filme.id" :filmeTitulo="filme.title" :filmeAno="filme.ano"/> -->
-                <!-- No entanto, se existir mais atributos o código ficará muito verboso. Então, a solução que o Vue oferece é utilizando a diretiva v-bind, porém as props devem ter o mesmo nome dos atributos. Ou sjea, 'filmeTitulo' deve passar a se chamar 'titulo', a mesma coisa para 'filmeAno' que deverá ser apenas 'ano' -->
-                <FilmesListaItem v-for="filme in filmes" :key="filme.id" v-bind="filme"/>
+                <!-- a class 'dark-theme' é adicionada automaticamente pelo Vue no atributo class do component em questão, ficando assim o li com 'list-group-item dark-theme' -->
+                <FilmesListaItem
+                    v-for="filme in filmes"
+                    :key="filme.id"
+                    v-bind="filme"
+                    class="dark-theme"
+                    placeholder="Título do filme"
+                />
             </ul>
         </div>
 
@@ -31,10 +35,10 @@ export default {
     data() {
         return {
             filmes: [
-                { id: 1, title: 'Capitão Marvel', year: 2019},
-                { id: 2, title: 'Vingadores: Guerra Infinita', year: 2019},
-                { id: 3, title: 'Homem-Aranha: Longe de Casa', year: 2019},
-                { id: 4, title: 'Viúva Negra', year: 2021}
+                { id: 1, title: 'Capitão Marvel', year: 2019, director: 'Stan Lee'},
+                { id: 2, title: 'Vingadores: Guerra Infinita', year: 2019, director: 'Stan Lee'},
+                { id: 3, title: 'Homem-Aranha: Longe de Casa', year: 2019, director: 'Stan Lee'},
+                { id: 4, title: 'Viúva Negra', year: 2021, director: 'Stan Lee'}
             ]
         }
     }
--- with-docker/code/vue-components-comunicacao/src/components/FilmesListaItem.vue
+++ with-docker/code/vue-components-comunicacao/src/components/FilmesListaItem.vue
@@ -1,12 +1,16 @@
 <template>
-    <li class="list-group-item">
+    <div>
+        <input type="text" :value="title" :placeholder="$attrs.placeholder">
+    </div>
+    <!-- <li class="list-group-item">
         <span>{{ filmeTituloConcat }} ({{ year }})</span>
         <button class="btn btn-success float-right">Selecionar</button>
-    </li>
+    </li> -->
 </template>
 
 <script>
 export default {
+    inheritAttrs: false, // Especifica se deseja herdar ou não os atributos do elemento root. Assim, podemos passar atributos customizáveis e utilizá-los nos elementos desejados. 
     props: {
         title: {
             type: String,
@@ -21,6 +25,9 @@ export default {
         filmeTituloConcat() {
             return `Titulo: ${this.title}`
         }
+    },
+    created() {
+        console.log('Attrs: ', this.$attrs);
     }
 }
 </script>
