1fbd9f53b13e21780edb063e8976a4170d4ee99d
Tairone Dias <taironecdias@gmail.com>
2020-11-12 18:40:43 -0300

    reaproveitando codigo e dados com Event Bus

--- with-docker/code/vue-components-comunicacao/src/components/FilmesListaItem.vue
+++ with-docker/code/vue-components-comunicacao/src/components/FilmesListaItem.vue
@@ -19,7 +19,8 @@ export default {
     methods: {
         chooseMovie() {
             // this.$emit('movieSelected', this.movie)
-            eventBus.$emit('movieSelected', this.movie)
+            // eventBus.$emit('movieSelected', this.movie)
+            eventBus.movieSelected(this.movie)
         }
     }
 }
--- with-docker/code/vue-components-comunicacao/src/main.js
+++ with-docker/code/vue-components-comunicacao/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-export const eventBus = new Vue()
+export const eventBus = new Vue({
+  methods: {
+    movieSelected(value) {
+      this.$emit('movieSelected', value)
+    }
+  }
+})
 
 new Vue({
   render: h => h(App),
