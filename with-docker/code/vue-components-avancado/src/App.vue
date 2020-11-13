15b45397a96c39cfbc36119909c7fd48e11cdee5
Tairone Dias <taironecdias@gmail.com>
2020-11-13 19:28:10 -0300

    criando o post component, passando dados para ele e customizando o template com slots

--- with-docker/code/vue-components-avancado/src/App.vue
+++ with-docker/code/vue-components-avancado/src/App.vue
@@ -1,5 +1,25 @@
 <template>
   <div id="app">
-    <h1>Vue Components Avançado</h1>
+    <h1>Vue JS</h1>
+
+    <Post titulo="Components no Vue">
+      <h3>Components no Vue.js</h3>
+      <p>Components são propriedades mais importantes no Vue.js</p>
+    </Post>
+
+    <Post titulo="Passando dados via props"/>
+
+
   </div>
 </template>
+
+<script>
+
+import Post from './components/Post';
+
+export default {
+  components: {
+    Post
+  }
+}
+</script>
---
+++ with-docker/code/vue-components-avancado/src/components/Post.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>{{ titulo }}</h2>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titulo: String
+    }
+}
+</script>
