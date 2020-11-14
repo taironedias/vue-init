8a4e74688239b6ee7b7272f41975eeae543ee256
Tairone Dias <taironecdias@gmail.com>
2020-11-13 21:10:54 -0300

    usando slots com escopo

--- with-docker/code/vue-components-avancado/src/App.vue
+++ with-docker/code/vue-components-avancado/src/App.vue
@@ -2,29 +2,49 @@
   <div id="app" class="container">
     <h1>Vue JS</h1>
 
-    <Post>
-      <h3 slot="cabecalho">Components no Vue.js</h3>
+    <h1>Forma Padrão</h1>
 
-      <p class="post-paragrafo">Components são instâncias reutilizáveis no Vue.js com nome. Componentes podem ser reutilizados quantas vezes você quiser.</p>
-      <p>...</p>
+    <PostsLista :posts="posts" />
 
-      <small slot="rodape">por {{ autor }}</small>
-    </Post>
+    <hr>
+    <h1>Slots com escopo</h1>
+
+    <PostsLista :posts="posts" >
+      <template slot-scope="slotProps">
+        <!-- {{slotProps}} -->
+        <h2>{{ slotProps.meuPost.titulo }}</h2>
+        <p>{{ slotProps.meuPost.conteudo }}</p>
+        <small>{{ slotProps.meuPost.autor }}</small>
+      </template>
+    </PostsLista>
 
   </div>
 </template>
 
 <script>
 
-import Post from './components/Post';
+import PostsLista from './components/PostsLista';
 
 export default {
   components: {
-    Post
+    PostsLista
   },
   data() {
     return {
-      autor: 'Tairone'
+      posts: [
+        { 
+          id: 1,
+          titulo: 'Components no Vue.js',
+          conteudo: 'Components são instâncias reutilizáveis no Vue.js com nome. Componentes podem ser reutilizados quantas vezes você quiser.',
+          autor: 'Tairone Dias'
+        },
+        { 
+          id: 2,
+          titulo: 'Slot no Vue.js',
+          conteudo: 'Vue implementa uma API de distribuição de conteúdo que é modelada após o atual detalhamento da especificação dos componentes da Web, usando o elemento <slot> para servir como saída de distribuição de conteúdos.',
+          autor: 'Tairone Dias'
+        }
+      ]
     }
   }
 }
---
+++ with-docker/code/vue-components-avancado/src/components/PostsLista.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Lista de Posts</h2>
+
+        <PostsListaIten v-for="item in posts" :key="item.id" :post="item">
+            <slot :meuPost="item"></slot>
+        </PostsListaIten>
+
+    </div>
+</template>
+
+<script>
+
+import PostsListaIten from './PostsListaIten';
+
+export default {
+    components: {
+        PostsListaIten
+    },
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
