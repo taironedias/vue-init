693c6d12551912ddda05d68c0d9735ca1d297bdf
Tairone Dias <taironecdias@gmail.com>
2020-11-16 20:14:49 -0300

    corrigindo identação

--- with-docker/code/vue-animacoes/src/App.vue
+++ with-docker/code/vue-animacoes/src/App.vue
@@ -1,29 +1,28 @@
 <template>
-  <div id="app">
+    <div id="app">
 
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Animações</h1>
-        <p class="lead">Treinando transição/animação de elementos/components no Vue.js</p>
-      </div>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Animações</h1>
+                <p class="lead">Treinando transição/animação de elementos/components no Vue.js</p>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="btn btn-primary mb-3" @click="mostrar = !mostrar">Mostrar</div>
+
+            <div class="alert alert-primary" v-if="mostrar">Animações em Vue.js</div>
+        </div>
+
     </div>
-
-    <div class="container">
-
-      <div class="btn btn-primary mb-3" @click="mostrar = !mostrar">Mostrar</div>
-
-      <div class="alert alert-primary" v-if="mostrar">Animações em Vue.js</div>
-    </div>
-
-  </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      mostrar: true
+    data() {
+        return {
+            mostrar: true
+        }
     }
-  }
 }
 </script>
