09a32f8b89b5d6d1abf74dd3e26a90289464e0ad
Tairone Dias <taironecdias@gmail.com>
2020-11-16 20:07:04 -0300

    init project

---
+++ with-docker/code/vue-animacoes/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Animações</h1>
+        <p class="lead">Treinando transição/animação de elementos/components no Vue.js</p>
+      </div>
+    </div>
+
+    <div class="container"></div>
+
+  </div>
+</template>
