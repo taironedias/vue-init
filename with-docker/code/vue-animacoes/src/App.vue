6fc84d0c00d9d3fb12bd0abd40a7858ce19ba59d
Tairone Dias <taironecdias@gmail.com>
2020-11-24 22:19:19 -0300

    animando troca de valores em numeros com Green Sock

--- with-docker/code/vue-animacoes/src/App.vue
+++ with-docker/code/vue-animacoes/src/App.vue
@@ -12,15 +12,16 @@
             <h3 class="font-weight-light">Animações de Estado</h3>
 
             <div class="form-group">
-                <input class="form-control" v-model="numero" step="50">
+                <input class="form-control" v-model="numero" @keyup.enter="deslocar">
             </div>
 
             <div class="alert alert-info">
                 <h3 class="font-weight-light">
                     <strong>Número: </strong>
-                    <span>{{ numero }}</span>
+                    <span id="test" ref="refTest">{{ numero }}</span>
                 </h3>
             </div>
+            <img ref="logo" src="./assets/logo.png" alt="Logo Vue.js">
 
         </div>
 
@@ -28,10 +29,25 @@
 </template>
 
 <script>
+
+import { gsap } from 'gsap';
+
 export default {
     data() {
         return {
-            numero: 0
+            numero: 0,
+            numeroInerpolado: 0
+        }
+    },
+    computed: {
+        numeroAnimado() {
+            return this.numeroInerpolado.toFixed(0)
+        }
+    },
+    methods: {
+        deslocar() {
+            console.log('this.$refs.refTest :>> ', this.$refs.refTest);
+            gsap.to(this.$refs.logo, {duration: 2, x: this.numero})
         }
     }
 }
