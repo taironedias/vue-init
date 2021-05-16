b4bae82f163bf89da55e3c00d39f500ebe482958
Tairone Dias <taironecdias@gmail.com>
2021-05-16 11:38:08 -0300

    alterando para history mode

--- with-docker/code/vue-rotas/src/main.js
+++ with-docker/code/vue-rotas/src/main.js
@@ -9,6 +9,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history', // em aplicações publicadas esse modo deve ter tratado no servidor para que sempre retorne o index, e assim o Vue continuar o tratamento das rotas no front-end
   routes: [
     { path: '/contatos', component: Contatos }, // meu-dominio.com/contatos
     { path: '/', component: Home } // meu-dominio.com/
