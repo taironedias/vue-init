330b76aff6fe3899500901f5052aad0c1c1d01d2
Tairone Dias <taironecdias@gmail.com>
2021-05-21 20:23:44 -0300

    aplicando css no router-link ativo

--- with-docker/code/vue-rotas/src/router.js
+++ with-docker/code/vue-rotas/src/router.js
@@ -8,7 +8,8 @@ Vue.use(VueRouter)
 
 export default new VueRouter({
   mode: 'history', // em aplicações publicadas esse modo deve ter tratado no servidor para que sempre retorne o index, e assim o Vue continuar o tratamento das rotas no front-end
-    routes: [
+  linkExactActiveClass: 'active', // adiciona class bootstrap para destacar o elemento a qual foi clicado para a rota. Vale ressaltar que o Vue possue sua própria class que é 'router-link-active', essa que foi utilizado é a do bootstrap
+  routes: [
     { path: '/contatos', component: Contatos }, // meu-dominio.com/contatos
     { path: '/', component: Home } // meu-dominio.com/
   ]
