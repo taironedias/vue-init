dd8974be6c416502220053a5b7ac90083652490d
Tairone Dias <taironecdias@gmail.com>
2021-05-23 10:20:31 -0300

    preparando código para usar parâmetroos de rota

---
+++ with-docker/code/vue-rotas/src/components/contatos/ContatosLista.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h3 class="font-weight-light">Contatos</h3>
+        <ul class="list-group" v-if="contatos.length > 0">
+            <ContatosListaIten
+                class="list-group-item"
+                v-for="contato in contatos"
+                :key="contato.id"
+                :contact="contato"
+            />
+        </ul>
+        <p v-else>Nenhum contato cadastrado.</p>
+        <button class="btn btn-secondary mt-4 mb-4" @click="voltarPagina">Voltar</button>
+    </div>
+</template>
+
+<script>
+import ContatosListaIten from './ContatosListaIten.vue'
+
+export default {
+    components: {
+        ContatosListaIten
+    },
+    data() {
+        return {
+            contatos: [
+                { id: 1, name: 'Danilo Gentili', email: 'danilo@gentili.com' },
+                { id: 2, name: 'Thiago Ventura', email: 'thiago@ventura.com' },
+                { id: 3, name: 'Rafael Portugal', email: 'rafael@portugal.com' }
+            ]
+        }
+    },
+    methods: {
+        voltarPagina() {
+            this.$router.back()
+        }
+    }
+}
+</script>
---
+++ with-docker/code/vue-rotas/src/components/contatos/ContatosListaIten.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        <span>{{ contact.name }}</span>
+        <button class="btn btn-info btn-sm float-right">Detalhes</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        contact: {
+            type: Object,
+            require: true
+        }
+    }
+}
+</script>
