a2528b0970a60436bae2c4bf14c3b8886512983e
Tairone Dias <taironecdias@gmail.com>
2021-05-23 10:45:20 -0300

    reagindo a alterações nos parâmetros das rotas com beforeRouteUpdate

--- with-docker/code/vue-rotas/src/views/contatos/ContatoDetalhes.vue
+++ with-docker/code/vue-rotas/src/views/contatos/ContatoDetalhes.vue
@@ -9,10 +9,14 @@ export default {
             id: this.$route.params.id
         }
     },
-    watch: {
+    /*watch: {
         '$route'(to, from) {
             this.id = to.params.id
         }
+    } */
+    beforeRouteUpdate(to, from, next) {
+        this.id = to.params.id
+        next()
     }
 }
 </script>
