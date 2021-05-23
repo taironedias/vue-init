39a64213df3414766defb5fb737208b0c5f06df2
Tairone Dias <taironecdias@gmail.com>
2021-05-23 11:16:52 -0300

    detalhes navegação programática

--- with-docker/code/vue-rotas/src/components/contatos/ContatosListaIten.vue
+++ with-docker/code/vue-rotas/src/components/contatos/ContatosListaIten.vue
@@ -6,6 +6,11 @@
             class="btn btn-info btn-sm float-right">
                 Detalhes
         </router-link>
+        <!-- <button
+            class="btn btn-info btn-sm float-right"
+            @click="verDetalhes">
+                Detalhes
+        </button> -->
     </li>
 </template>
 
@@ -15,6 +20,15 @@ export default {
         contact: {
             type: Object,
             require: true
+        }
+    },
+    methods: {
+        verDetalhes() {
+            // this.$router.push(`/contatos/${this.contact.id}`)   // forma que adiciona na pilha de navegação
+            // this.$router.push({ path: `/contatos/${this.contact.id}`})   // variação da forma acima porém com outra sintaxe
+            // this.$router.push({ path: '/contatos', params: { id: this.contact.id } })   // utilizando o 'params' essa forma NÃO FUNCIONA pois a rota está nomeada, então a forma correta é o código abaixo
+            // this.$router.push({ name: 'contato', params: { id: this.contact.id } })   // utilizando o 'params' essa forma NÃO FUNCIONA pois a rota está nomeada, então a forma correta é o código abaixo
+            // this.$router.replace({ name: 'contato', params: { id: this.contact.id } })   // uma outra variação que substitui a rota na pilha de navegação
         }
     }
 }
