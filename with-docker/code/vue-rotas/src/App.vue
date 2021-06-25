706279e024f8a44d24d8632ef68fc66a46d3b5fb
Tairone Dias <taironecdias@gmail.com>
2021-06-25 14:51:02 -0300

    animando navegação com transition

--- with-docker/code/vue-rotas/src/App.vue
+++ with-docker/code/vue-rotas/src/App.vue
@@ -21,7 +21,19 @@
           CONTATOS
       </router-link>
 
-      <router-view />
+      <transition name="slide" mode="out-in">
+        <router-view/>
+      </transition>
     </div>
   </div>
 </template>
+
+<style scoped>
+  .slide-enter, .slide-leave-to {
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+  .slide-enter-active, .slide-leave-active {
+    transition: all 0.3s;
+  }
+</style>
