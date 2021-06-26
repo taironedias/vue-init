7c1864af4bb809f25ed485e8d45b161f1c935732
Tairone Dias <taironecdias@gmail.com>
2021-06-26 17:43:58 -0300

    interceptando requisições

--- with-docker/code/vue-http/src/axios.js
+++ with-docker/code/vue-http/src/axios.js
@@ -1,6 +1,33 @@
 import axios from 'axios'
 import config from './config/config'
 
-export default axios.create({
-    baseURL: config.apiUrl
+const instance = axios.create({})
+
+instance.defaults.baseURL = config.apiUrl
+
+instance.interceptors.request.use(config => {
+    console.log('Interceptando requisições :>> ', config)
+    config.data = {
+        ...config.data,
+        author: 'Tairone Dias'
+    }
+
+    // async
+    // return new Promise((resolve) => {
+    //     console.log('Simulando uma requisição async')
+    //     setTimeout(() => {
+    //         console.log('Enviando uma requisição')
+    //         resolve(config)
+    //     }, 5000);
+    // })
+
+    // sync
+    return config
+
+}, error => {
+    /* Útil para algum tratamento global de manipulação de erro da aplicação */
+    console.log('Erro ao fazer requisição :>> ', error)
+    return Promise.reject(error)
 })
+
+export default instance
