ef3076a855b315ae568d781ce23b38e8eefce49f
Tairone Dias <taironecdias@gmail.com>
2021-06-26 00:22:52 -0300

    editando os contatos

--- with-docker/code/vue-rotas/src/views/contatos/ContatoEditar.vue
+++ with-docker/code/vue-rotas/src/views/contatos/ContatoEditar.vue
@@ -1,41 +1,78 @@
 <template>
-    <div>
-        <h3 class="font-weight-light">Editar Contato (ID: {{ id }})</h3>
-        <button
-            class="btn btn-secondary mt-4 mb-4"
-            @click="$router.back()">
-                Voltar
-        </button>
+    <div v-if="contact">
+        <h3 class="font-weight-light">Nome: {{ contact.name }}</h3>
+
+        <form @submit.prevent="save">
+            <div class="form-group">
+                <label>Nome</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Insira o nome"
+                    autofocus
+                    v-model="contact.name">
+            </div>
+            <div class="form-group">
+                <label>E-mail</label>
+                <input
+                    type="email"
+                    class="form-control"
+                    placeholder="Insira o e-mail"
+                    v-model="contact.email">
+            </div>
+
+            <div class="alert alert-danger mt-3" v-if="messageError">{{ messageError }}</div>
+
+            <button
+                class="btn btn-secondary mt-4 mb-4 mr-2"
+                @click="$router.back()">
+                    Voltar
+            </button>
+            <button
+                type="submit"
+                class="btn btn-success">
+                    Salvar
+            </button>
+        </form>
     </div>
 </template>
 
 <script>
+import EventBus from '@/event-bus'
+
 export default {
     props: ['id'],
     data() {
         return {
-            curso: 'Curso Vue.js'
+            contact: undefined,
+            messageError: '',
+            isSubmit: false
         }
     },
     beforeRouteEnter(to, from, next) {
-        console.log('beforeRouteEnter');
-        // console.log('this.curso :>> ', this.curso); // nesse momento, a instância Vue ainda não foi criada. Para executar qualquer instrução que necessite de algo dessa instância é importante implementar um callback para ser chamado pela função next, como segue abaixo
-        // if(to.query.auth === 'true') {
-        //     return next(vm => {
-        //         console.log('vm.curso :>> ', vm.curso);
-        //     })
-        // }
-
-        // next('/contatos')
-        next()
+        console.log('beforeRouteEnter')
+        next(vm => {
+            vm.contact = EventBus.searchContact(+to.params.id)
+        })
     },
     beforeRouteLeave(to, from, next) {
         console.log('beforeRouteLeave')
-        const confirmar = window.confirm('Deseja realmente sair da edição de contatos?')
-        next(confirmar)
+        this.isSubmit
+            ? next(window.confirm('Deseja realmente sair da edição de contatos?'))
+            : next()
     },
-    created() {
-        console.log('this.$route.params :>> ', this.$route.params);
+    methods: {
+        save() {
+            const r = EventBus.updateContact(this.contact)
+            this.isSubmit = true;
+
+            if (!r) {
+                this.messageError = `Ocorreu um erro ao salvar o contato: ${this.contact.name}`
+                return
+            }
+
+            this.$router.push('/contatos')
+        }
     }
 }
 </script>
