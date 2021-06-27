7eb68acc0872321923ffb963e7af40385d50299e
Tairone Dias <taironecdias@gmail.com>
2021-06-26 23:22:56 -0300

    trabalhando com requisições simultâneas

--- with-docker/code/vue-http/src/App.vue
+++ with-docker/code/vue-http/src/App.vue
@@ -14,11 +14,41 @@
 </template>
 
 <script>
+import axios from 'axios'
+import config from './config/config'
+
 import TarefasLista from './components/TarefasLista.vue'
 
 export default {
   components: {
     TarefasLista
+  },
+  created() {
+    /* Primeira forma utilizando o axios */
+    // axios.all([
+    //   axios.get(`${config.apiUrl}/tasks/1`),
+    //   axios.get(`${config.apiUrl}/tasks/3`),
+    //   axios.get(`${config.apiUrl}/tasks/4`)
+    // ]).then(axios.spread((responseTask1, responseTask3, responseTask4) => {
+    //   console.log('Requisições simultâneas...')
+    //   console.log('Response da Task1 :>> ', responseTask1)
+    //   console.log('Response da Task3 :>> ', responseTask3)
+    //   console.log('Response da Task4 :>> ', responseTask4)
+    // }))
+
+    /* Segunda forma utilizando o ES6 */
+    axios.all([
+      axios.get(`${config.apiUrl}/tasks/1`),
+      axios.get(`${config.apiUrl}/tasks/3`),
+      axios.get(`${config.apiUrl}/tasks/4`)
+    ]).then(response => {
+      console.log('Requisições simultâneas...')
+      /* realizando a desestruturação na atribuição (destructuring assignment) */
+      const [responseTask1, responseTask3, responseTask4] = response
+      console.log('Response da Task1 :>> ', responseTask1)
+      console.log('Response da Task3 :>> ', responseTask3)
+      console.log('Response da Task4 :>> ', responseTask4)
+    })
   }
 }
 </script>
