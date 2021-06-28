e6ddc15364bfc75f5eddbd41dad9d852204bb92d
Tairone Dias <taironecdias@gmail.com>
2021-06-28 16:24:25 -0300

    init

---
+++ with-docker/code/vue-vuex/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Vuex</h1>
+        <p class="lead">Padrão de Gerenciamento de Estado Centralizado no Vue</p>
+      </div>
+    </div>
+
+    <div class="container">
+
+    </div>
+  </div>
+</template>
