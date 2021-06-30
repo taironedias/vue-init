f9ece81817aafb6f0d41cefbb5853edd0d8b1050
Tairone Dias <taironecdias@gmail.com>
2021-06-30 20:58:13 -0300

    usando actions para executar código assíncrono

--- with-docker/code/vue-vuex/src/components/TarefasLista.vue
+++ with-docker/code/vue-vuex/src/components/TarefasLista.vue
@@ -65,7 +65,7 @@ export default {
         ...mapGetters(['doneTasks', 'todoTasks', 'totalDoneTasks'])
     },
     created() {
-        setTimeout(() => {
+        // setTimeout(() => {
             /* primeira forma */
             // this.$store.dispatch('listTasks', {
             //     tasks: [
@@ -76,16 +76,25 @@ export default {
             // })
 
             /* segunda forma */
-            this.$store.dispatch({
-                type: 'listTasks',
-                tasks: [
-                    { id: 1, title: 'Aprender Vue.js', done: true },
-                    { id: 2, title: 'Aprender Vue Router', done: true },
-                    { id: 3, title: 'Aprender Vuex', done: false },
-                    { id: 4, title: 'Aprender JavaScript', done: true }
-                ]
-            })
-        }, 2000);
+            // this.$store.dispatch({
+            //     type: 'listTasks',
+            //     tasks: [
+            //         { id: 1, title: 'Aprender Vue.js', done: true },
+            //         { id: 2, title: 'Aprender Vue Router', done: true },
+            //         { id: 3, title: 'Aprender Vuex', done: false },
+            //         { id: 4, title: 'Aprender JavaScript', done: true }
+            //     ]
+            // })
+        // }, 2000);
+        this.$store.dispatch({
+            type: 'listTasks',
+            tasks: [
+                { id: 1, title: 'Aprender Vue.js', done: true },
+                { id: 2, title: 'Aprender Vue Router', done: true },
+                { id: 3, title: 'Aprender Vuex', done: false },
+                { id: 4, title: 'Aprender JavaScript', done: true }
+            ]
+        })
     },
     methods: {
         // ...mapMutations(['listTasks']),  // primeira forma
--- with-docker/code/vue-vuex/src/store.js
+++ with-docker/code/vue-vuex/src/store.js
@@ -44,8 +44,12 @@ export default new Vuex.Store({
          * 1. a action executa sempre uma mutantions, ela nunca deve implementar e alterar um estado diretamente
          * 2. action deve ser usando quando há requisições assíncronas, diferente da mutation que não devemos jamais chamá-las em requisições assíncronos
          */
-        listTasks: (context, payload) => {
-            context.commit('listTasks', payload)
+        listTasks: ({ commit }, payload) => {
+            console.log('action chamada...')
+            setTimeout(() => {
+                console.log('action executada')
+                commit('listTasks', payload)
+            }, 2000);
             /* o primeiro parâmetro é o nome da mutation que definir e o segundo é o payload */
         }
     }
