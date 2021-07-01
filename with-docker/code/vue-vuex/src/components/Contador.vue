72630d870e017db5fd7911201425a47e6dbdb976
Tairone Dias <taironecdias@gmail.com>
2021-06-30 21:42:33 -0300

    acessando state de módulos em components

--- with-docker/code/vue-vuex/src/components/Contador.vue
+++ with-docker/code/vue-vuex/src/components/Contador.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h2 class="font-weight-light">Contador: {{ contador }}</h2>
-        <h4 class="font-weight-light">Contador 2x: {{ contadorMultiplicado }}</h4>
+        <h4 class="font-weight-light">Contador x2: {{ contadorMultiplicado }}</h4>
         <h4 class="font-weight-light">Contador Alias: {{ contadorAlias }}</h4>
         <button class="btn btn-lg btn-danger mr-1" @click="decrementar">
             <i class="fa fa-minus"></i>
@@ -24,21 +24,21 @@ export default {
     // computed: mapState(['contador']),    // primeira forma
     computed: {
         ...mapState({                    // segunda forma
-            contador: state => state.contador,  // variação 1
+            contador: state => state.contador.contador,  // variação 1
             contadorMultiplicado(state) {       // variação 1.1
                 /* utilizando essa variação quando é necessário acessar algum estado de propriedade local da instância Vue do component */
-                return state.contador * this.constLocal
+                return state.contador.contador * this.constLocal
             },
-            contadorAlias: 'contador'           // variação 2
+            contadorAlias: state => state.contador.contador           // variação 2
         })
         // outras computed properties do component
     },
     methods: {
         decrementar() {
-            this.$store.state.contador--
+            this.$store.state.contador.contador--
         },
         incrementar() {
-            this.$store.state.contador++
+            this.$store.state.contador.contador++
         }
     }
 }
