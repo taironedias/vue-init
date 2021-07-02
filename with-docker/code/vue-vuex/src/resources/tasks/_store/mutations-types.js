6e40a1ec70defac063c61b8cb89932ba63b875a6
Tairone Dias <taironecdias@gmail.com>
2021-07-02 10:24:00 -0300

    implementando Mutations do tasks module

--- with-docker/code/vue-vuex/src/resources/tasks/_store/mutations-types.js
+++ with-docker/code/vue-vuex/src/resources/tasks/_store/mutations-types.js
@@ -1,1 +1,4 @@
+export const CREATE_TASKS = 'createTasks'
+export const EDIT_TASKS = 'editTasks'
+export const DELETE_TASKS = 'deleteTasks'
 export const LIST_TASKS = 'listTasks'
--- with-docker/code/vue-vuex/src/resources/tasks/_store/mutations.js
+++ with-docker/code/vue-vuex/src/resources/tasks/_store/mutations.js
@@ -1,6 +1,17 @@
-import { LIST_TASKS } from './mutations-types'
+import { CREATE_TASKS, EDIT_TASKS, DELETE_TASKS, LIST_TASKS } from './mutations-types'
 
 export default {
+    [CREATE_TASKS]: (state, { task }) => {
+        state.tasks.push(task)
+    },
+    [EDIT_TASKS]: (state, { task }) => {
+        const index = state.tasks.findIndex(t => t.id === task.id)
+        state.tasks.splice(index, 1, task)
+    },
+    [DELETE_TASKS]: (state, id) => {
+        const index = state.tasks.findIndex(t => t.id === id)
+        state.tasks.splice(index, 1)
+    },
     [LIST_TASKS]: (state, { tasks }) => {
         state.tasks = tasks
     }
