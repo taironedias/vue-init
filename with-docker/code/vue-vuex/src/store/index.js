3f7a3d9127dbef45d6a951caf4b749a471d7d971
Tairone Dias <taironecdias@gmail.com>
2021-07-02 10:06:14 -0300

    habilitando strict mode para ambientes !production

--- with-docker/code/vue-vuex/src/store/index.js
+++ with-docker/code/vue-vuex/src/store/index.js
@@ -30,6 +30,7 @@ const modules = {
 }
 
 export default new Vuex.Store({
+    strict: process.env.NODE_ENV !== 'production',   // essa config serve para disparar erro no console caso haja alteração direta no state (fora da mutation), porém só está sendo ativada fora do modo de produção, ou seja, nos modos 'development' e 'test' (apenas no teste unitário), pois com essa configuração como true a performance cai pois há um observador sendo executada a cada alteração no estado de cada módulo
     state,
     getters,
     mutations,
